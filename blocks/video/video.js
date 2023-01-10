31d1707f39a760a4e0834683a405a9c7f8c36be1
Benedikt Braun <b.braun@techdivision.com>
2023-01-10 13:48:56 +0100

    video block added

---
+++ blocks/video/video.js
@@ -0,0 +1,11 @@
+export default async function decorate(block) {
+  const a = block.querySelector('a');
+  if (a) {
+    const source = a.href;
+    block.innerHTML = `
+    <video controls>
+      <source src="${source}" type="video/${source.split('.').pop()}" >
+    </video>
+    `;
+  }
+}
